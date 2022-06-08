38a20d550d0cd7cd645de7fa0c8f23412f0e863e
Karen <mansheung36@gmail.com>
2022-06-09 01:41:09 +0800

    feature: todo list css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="App-margin">
+        <div className='App-context'>
+          <div className='App-headline'>
+            <a className='App-heading' href='/'>
+              <div className='App-logo-context'>
+                <img src='https://stickylist.app/img/logo.svg' width='32' alt='StickyList brand'></img>
+                <h1 className='App-title'> StickyList </h1>
+              </div>
+            </a>
+          </div>
+
+          <div className='App-box rounded'>
+
+            <div class="padding-top-left-right-20">
+              <input type="text" className="input-text " placeholder="Add New Item here (Eg. Implement Dark Mode)" />
+              <button class="add-todo-button" type="button" >Add Todo</button>
+            </div>
+
+
+            <div mode="dark" className='margin-20'>
+              <div className='welcome-block'>
+                Welcome to your StickyList.<br />
+                <br />
+                You have no items on the list. Add some items to your sticky list and stay focussed.<br />
+              </div>
+              <div className='text-align-center'>
+                <img className='workdesk-img' src='	https://stickylist.app/img/workdesk-dark.png'></img>
+              </div>
+            </div>
+
+          </div>
+
+          <div className='git-link'>
+            Git
+          </div>
+        </div>
+
+      </div>
+
     </div>
+
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossOrigin="anonymous"></link>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossOrigin="anonymous"></script>
+
     <App />
   </React.StrictMode>
 );
