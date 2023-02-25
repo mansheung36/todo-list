dad1030bf63048d7db78fb8cbb5110981567dbe0
Karen Wong <mansheung36@gmail.com>
2023-02-25 18:23:57 +0800

    chore: format code

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
-import './App.css';
-import TodoListContainer from './TodoListContainer';
-import Darkmode from './Darkmode';
+import "./App.css";
+import TodoListContainer from "./TodoListContainer";
+import Darkmode from "./Darkmode";
 
 function App() {
-
-
   return (
-
     <div className="App">
-
       <div className="App-margin">
-        <div className='App-context'>
-          <div className='App-headline'>
-            <a className='App-heading' href='/'>
-              <div className='App-logo-context'>
-                <img src='https://stickylist.app/img/logo.svg' width='32' alt='StickyList brand'></img>
-                <h1 className='App-title'> StickyList </h1>
+        <div className="App-context">
+          <div className="App-headline">
+            <a className="App-heading" href="/">
+              <div className="App-logo-context">
+                <img
+                  src="https://stickylist.app/img/logo.svg"
+                  width="32"
+                  alt="StickyList brand"
+                ></img>
+                <h1 className="App-title"> StickyList </h1>
               </div>
             </a>
             <Darkmode></Darkmode>
           </div>
 
-          <div className='App-box rounded'>
-
+          <div className="App-box rounded">
             <TodoListContainer />
-
-
-
           </div>
 
-          <div className='git-link'>
-            Pratice reference from: <a href='https://stickylist.app/'>https://stickylist.app/</a>
+          <div className="git-link">
+            Pratice reference from:{" "}
+            <a href="https://stickylist.app/">https://stickylist.app/</a>
           </div>
+          
         </div>
-
       </div>
-
     </div>
-
-
   );
 }
-
 export default App;
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,29 +1,30 @@
-import React from 'react'
+import React from "react";
 
-
-
-
-export default function TodoItem({ todo, handleDeleteTodo, handleCompletedTodo }) {
-    return (
-
-
-        <li className=" todo-item row " data-todo-id={todo.id} >
-
-
-            <div className=" col-8 todo-content" onClick={() => { handleCompletedTodo(todo.id) }}>{todo.content}</div>
-            <div className='col-1'>
-
-                
-                    <img src='	https://stickylist.app/img/trash-light.svg' alt="" className='trash-img' onClick={() => { handleDeleteTodo(todo.id) }} />
-           
-
-            </div>
-
-
-
-
-
-        </li>
-
-    )
+export default function TodoItem({
+  todo,
+  handleDeleteTodo,
+  handleCompletedTodo,
+}) {
+  return (
+    <li className=" todo-item row " data-todo-id={todo.id}>
+      <div
+        className=" col-8 todo-content"
+        onClick={() => {
+          handleCompletedTodo(todo.id);
+        }}
+      >
+        {todo.content}
+      </div>
+      <div className="col-1">
+        <img
+          src="	https://stickylist.app/img/trash-light.svg"
+          alt=""
+          className="trash-img"
+          onClick={() => {
+            handleDeleteTodo(todo.id);
+          }}
+        />
+      </div>
+    </li>
+  );
 }
